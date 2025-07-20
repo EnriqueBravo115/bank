2f2dac48bf3d38e844b55b4d55b617ea061f445b
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-07-20 13:58:38 -0600

    add api-gateway and config-server

--- config-server/src/main/java/dev/enrique/bank/ConfigServerApplication.java
+++ config-server/src/main/java/dev/enrique/bank/ConfigServerApplication.java
@@ -2,7 +2,11 @@ package dev.enrique.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
     public static void main(String[] args) {
