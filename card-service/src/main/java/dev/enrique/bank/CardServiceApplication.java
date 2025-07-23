352a48707d277a9207a28d7010d190958d5138b1
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-07-23 17:48:26 -0600

    add card-service & transaction-service

--- card-service/src/main/java/dev/enrique/bank/CardServiceApplication.java
+++ card-service/src/main/java/dev/enrique/bank/CardServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
+public class CardServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CardServiceApplication.class, args);
+	}
 }
--- transaction-service/src/main/java/dev/enrique/bank/TransactionServiceApplication.java
+++ transaction-service/src/main/java/dev/enrique/bank/TransactionServiceApplication.java
@@ -2,12 +2,10 @@ package dev.enrique.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+public class TransactionServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
 }
