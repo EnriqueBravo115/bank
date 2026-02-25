441a4e9afacb22f57a8344d51f31ec20f51e2c7f
EnriqueBravo115 <enriquebravo115@gmail.com>
2026-02-25 11:19:49 -0600

    disable dataSource auto-configuration in eureka-server

--- eureka-server/src/main/java/dev/enrique/bank/EurekaServerApplication.java
+++ eureka-server/src/main/java/dev/enrique/bank/EurekaServerApplication.java
@@ -2,12 +2,13 @@ package dev.enrique.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class EurekaServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
