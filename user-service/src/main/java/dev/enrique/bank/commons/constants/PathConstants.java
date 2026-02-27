451df4129245a172599e0d1453527b661b361ada
EnriqueBravo115 <enriquebravo115@gmail.com>
2026-02-27 12:09:59 -0600

    delete old paths related to old authentication

--- user-service/src/main/java/dev/enrique/bank/commons/constants/PathConstants.java
+++ user-service/src/main/java/dev/enrique/bank/commons/constants/PathConstants.java
@@ -6,20 +6,5 @@ public class PathConstants {
 
     public static final String API = "/api/v1";
     public static final String USER = "/user";
-    public static final String AUTH = API + USER + "/auth";
     public static final String REGISTER = API + USER + "/register";
-
-    // Controller AUTH
-    public static final String LOGIN = "/login";
-    public static final String FORGOT = "/forgot";
-    public static final String RESET = "/reset";
-    public static final String RESET_CODE = "/reset/{code}";
-    public static final String RESET_CURRENT = "/reset/current";
-    public static final String USER_EMAIL = "/user/{email}";
-
-    // Controller REGISTER
-    public static final String REGISTER_CHECK = REGISTER + "/check";
-    public static final String REGISTER_CODE = REGISTER + "/code";
-    public static final String REGISTER_ACTIVATE = REGISTER + "/activate/{code}";
-    public static final String REGISTER_CONFIRM = REGISTER + "/confirm";
 }
