de4bfeab18bfb394bd648c2ed44e524d084073a5
EnriqueBravo115 <enriquebravo115@gmail.com>
2026-03-15 14:02:33 -0600

    refactor(user-service): remove unused utilities and unimplemented settings service

--- user-service/src/main/java/dev/enrique/bank/commons/util/BasicMapper.java
+++ user-service/src/main/java/dev/enrique/bank/commons/util/BasicMapper.java
@@ -1,8 +1,5 @@
 package dev.enrique.bank.commons.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,5 @@ public class BasicMapper {
 
     public <T, S> S convertToResponse(T data, Class<S> type) {
         return mapper.map(data, type);
-    }
-
-    public <T, S> List<S> convertToResponseList(List<T> lists, Class<S> type) {
-        return lists.contains(null) ? new ArrayList<>()
-                : lists.stream()
-                        .map(list -> convertToResponse(list, type))
-                        .toList();
     }
 }
