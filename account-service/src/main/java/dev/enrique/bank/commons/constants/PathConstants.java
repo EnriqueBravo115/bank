a90436d8b222269e08088cdbd359a9bf66a6cbdf
EnriqueBravo115 <enriquebravo115@gmail.com>
2026-03-22 14:26:24 -0600

    feat(kong): register account-service in kong

--- account-service/src/main/java/dev/enrique/bank/commons/constants/PathConstants.java
+++ account-service/src/main/java/dev/enrique/bank/commons/constants/PathConstants.java
@@ -3,6 +3,5 @@ package dev.enrique.bank.commons.constants;
 public class PathConstants {
     public static final String AUTH_USER_ID_HEADER = "X-auth-user-id";
     public static final String API = "/api/v1";
-    public static final String USER = "/api/v1/user";
-    public static final String ACCOUNT = USER + "/account";
+    public static final String ACCOUNT = API + "/account";
 }
